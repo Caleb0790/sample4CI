ce5ca45cfb6d3cd178c0a2be2cab0fb856badc7e
cjoseph <cjoseph@MTP-D3020-ROU008.DIGINAMIC.LOCAL>
2017-11-23 15:05:49 +0100

    new push

---
+++ src/main/java/main/User.java
@@ -0,0 +1,49 @@
+package main;
+
+public class User {
+
+    public static Long id;
+
+    private String lastName;
+
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = "lastName=" + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+    	return super.hashCode();
+    }
+    
+    @Override
+    public String toString() {
+        if (id == null) {
+            return "";
+        }
+        return "id=" + id + ", lastName=" + lastName + ", firstName=" + firstName;
+    }
+}
